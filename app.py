3ad1fff6fccb092c1fe1af546ba101d6794de594
DevRaphael7 <raphaelramalho1989@gmail.com>
2022-10-26 20:20:34 -0300

    Add cors for mu aplication

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from controllers.calculator import CalculatorApi
 
 app = Flask(__name__)
+CORS(app)
 
 api = Api(app)
 
